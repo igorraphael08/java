74fd98e853e1a2557cf8c36f0bf9adfe89854dd9
igor raphael <169068315+igorraphael08@users.noreply.github.com>
2024-09-09 11:21:14 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    int contador = 1;
+while (  contador < 11) {
+
+System.out.println("bom dia! ");
+
+  contador = contador + 1;
+}
+
+
+System.out.println(nome);
+    
+  }
+
+  
+}
